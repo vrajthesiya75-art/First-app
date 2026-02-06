735f91916743d55c9234013bd1b9b07f569422ef
venusapps-prem <intern@venusapps.com>
2026-02-06 19:10:00 +0530

    chenge

--- myapp/src/utils/staffUtils.js
+++
@@ -1,7 +0,0 @@
-// src/utils/staffUtils.js   (optional – or just paste it where needed)
-export const renumberStaffIds = (staffArray) => {
-  return staffArray.map((item, index) => ({
-    ...item,
-    id: index + 1
-  }));
-};
